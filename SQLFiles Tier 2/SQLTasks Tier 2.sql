ee04c1f274f51dd1fa77b3f94f1bb8403f51472c
kunz <tanner.kunz22@gmail.com>
2021-11-09 00:31:36 -0700

    Finished Country Club SQL assignment.

--- SQLFiles Tier 2/SQLTasks Tier 2.sql
+++ SQLFiles Tier 2/SQLTasks Tier 2.sql
@@ -3,7 +3,7 @@ the PHPMyAdmin interface, and partly in Jupyter via a Python connection.
 
 This is Tier 2 of the case study, which means that there'll be less guidance for you about how to setup
 your local SQLite connection in PART 2 of the case study. This will make the case study more challenging for you: 
-you might need to do some digging, aand revise the Working with Relational Databases in Python chapter in the previous resource.
+you might need to do some digging, and revise the Working with Relational Databases in Python chapter in the previous resource.
 
 Otherwise, the questions in the case study are exactly the same as with Tier 1. 
 
@@ -31,39 +31,68 @@ Before starting with the questions, feel free to take your time,
 exploring the data, and getting acquainted with the 3 tables. */
 
 
-/* QUESTIONS 
+/* QUESTIONS */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
+SELECT name 
+FROM Facilities 
+WHERE membercost > 0;
 
 /* Q2: How many facilities do not charge a fee to members? */
 
+SELECT COUNT(*) AS num_nofee 
+FROM Facilities 
+WHERE membercost = 0;
+
+/* 4 no fee facilities */
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities 
+WHERE membercost < (0.2 * monthlymaintenance);
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
 
+SELECT *
+FROM Facilities
+WHERE facid IN (1, 5);
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT name, monthlymaintenance, 
+       CASE WHEN monthlymaintenance > 100 THEN 'expensive'
+            ELSE 'cheap' END AS mm_labels
+FROM Facilities;
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
+SELECT firstname, surname
+FROM Members
+ORDER BY joindate DESC;
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
+SELECT DISTINCT CONCAT_WS(" ", m.firstname, m.surname) AS member, f.name
+FROM Bookings AS b
+     INNER JOIN Facilities AS f
+     USING (facid)
+     INNER JOIN Members AS m
+     USING (memid)
+WHERE b.facid IN (0, 1)
+ORDER BY member;
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
@@ -72,9 +101,36 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
+SELECT f.name, 
+       CONCAT_WS(" ", m.firstname, m.surname) AS member, 
+       CASE WHEN memid = 0 THEN b.slots * f.guestcost
+            ELSE b.slots * f.membercost END AS cost
+FROM Bookings AS b
+     INNER JOIN Facilities AS f
+     USING (facid)
+     INNER JOIN Members AS m
+     USING (memid)
+WHERE starttime LIKE "%2012-09-14%"
+	  AND ((memid = 0 AND b.slots * f.guestcost > 30)
+           OR (memid <> 0 AND b.slots * f.membercost > 30))
+ORDER BY cost DESC;
+
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
+SELECT name, member, cost
+FROM (SELECT f.name, 
+             CONCAT_WS(" ", m.firstname, m.surname) AS member, 
+             CASE WHEN memid = 0 THEN b.slots * f.guestcost
+                  ELSE b.slots * f.membercost END AS cost
+      FROM Bookings AS b
+           INNER JOIN Facilities AS f
+           USING (facid)
+           INNER JOIN Members AS m
+           USING (memid)
+      WHERE starttime LIKE "%2012-09-14%") AS costs
+WHERE cost > 30
+ORDER BY cost DESC;
 
 /* PART 2: SQLite
 
